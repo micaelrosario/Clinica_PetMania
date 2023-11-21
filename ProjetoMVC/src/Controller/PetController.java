db35733789bdbb6ca959375babc7596f59d7c3be
Micael Rosário <micaeldevrosario@gmail.com>
2023-11-21 15:03:53 -0300

    Menu Cadastro 90% Concluída rsrsr

--- ProjetoMVC/src/Controller/PetController.java
+++ ProjetoMVC/src/Controller/PetController.java
@@ -13,6 +13,7 @@ import Model.Pet;
 import View.SubMenuCadastro;
 import java.io.Serializable;
 import java.util.ArrayList;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 /**
@@ -32,18 +33,26 @@ public class PetController implements Serializable{
     public void cadastrarPet(){
         //Pegar Pet da view
         Pet pet = helper.obterModelo();
+        if (pet != null) {
+            PetDados petDados = new PetDados();
+            petDados.cadastrarPet(pet);
+            ImageIcon icon = new ImageIcon("C:\\Users\\Usuário\\OneDrive\\Documentos\\MeusProjetos-Github\\Clinica_PetMania\\ProjetoMVC\\src\\Imagens/sucess.png");
+            ImageIcon resizedIcon = new ImageIcon(icon.getImage().getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH));
+            JOptionPane.showMessageDialog(null, "Pet Cadastrado com Sucesso!","Info",JOptionPane.PLAIN_MESSAGE, resizedIcon);
+        }
         
-        PetDados petDados = new PetDados();
-        petDados.cadastrarPet(pet);
-    }
-    public void voltarAoMenu(){
-        SubMenuCadastro novoFrame = new SubMenuCadastro();
-        novoFrame.setVisible(true);
     }
     
     public void atualizaDono(){
         // Buscar Clientes do Banco de Dados
-        JOptionPane.showMessageDialog(null, "ComboBox Preenchido!");
         helper.preencherDonos();
+    }
+    
+    public void voltarSubMenuCadastro(){
+        // Ação após clicar no Botão Voltar
+        SubMenuCadastro novoFrame = new SubMenuCadastro();
+        novoFrame.setVisible(true);
+        // Feche a janela de login
+        this.view.dispose();
     }
 }
